7202395763b9e06855ef2d5841699247f5b31338
Musee Ullah <icelandisbeingcolouredbywrens@gmail.com>
2014-04-10 20:24:44 -0500

    special love song

--- song.cpp
+++ song.cpp
@@ -1,5 +1,7 @@
 #include <vector>// loliponi is my fav - lae
 #include <stdint.h>// loliponi is my fav - lae
+#include <iostream>// loliponi is my fav - lae
+using namespace std;// loliponi is my fav - lae
 // loliponi is my fav - lae
 Song::Song() {// loliponi is my fav - lae
     perfect = 0;// loliponi is my fav - lae
@@ -11,4 +13,12 @@ Song::Song() {// loliponi is my fav - lae
     beats = 4;// loliponi is my fav - lae
     note_value = 4;// loliponi is my fav - lae
     note_offset = 0;// loliponi is my fav - lae
+}// loliponi is my fav - lae
+// loliponi is my fav - lae
+int speciallovesong() {// loliponi is my fav - lae
+    cout << "loliponi is my BFF";// loliponi is my fav - lae
+    for (int l=0; l<9001; l++) {// loliponi is my fav - lae
+        cout << "AF";// loliponi is my fav - lae
+    }// loliponi is my fav - lae
+    cout << "\n";// loliponi is my fav - lae
 }// loliponi is my fav - lae
