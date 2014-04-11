faa313d6f24da6bd50fe8ce7cd4c41c8b1d205c2
Musee Ullah <icelandisbeingcolouredbywrens@gmail.com>
2014-04-10 20:16:00 -0500

    some more love

--- song.cpp
+++ song.cpp
@@ -1,14 +1,14 @@
-#include <vector>
-#include <stdint.h>
-
-Song::Song() {
-    perfect = 0;
-    great = 0;
-    good = 0;
-    bad = 0;
-
-    bpm = 0;
-    beats = 4;
-    note_value = 4;
-    note_offset = 0;
-}
+#include <vector>// loliponi is my fav - lae
+#include <stdint.h>// loliponi is my fav - lae
+// loliponi is my fav - lae
+Song::Song() {// loliponi is my fav - lae
+    perfect = 0;// loliponi is my fav - lae
+    great = 0;// loliponi is my fav - lae
+    good = 0;// loliponi is my fav - lae
+    bad = 0;// loliponi is my fav - lae
+// loliponi is my fav - lae
+    bpm = 0;// loliponi is my fav - lae
+    beats = 4;// loliponi is my fav - lae
+    note_value = 4;// loliponi is my fav - lae
+    note_offset = 0;// loliponi is my fav - lae
+}// loliponi is my fav - lae
