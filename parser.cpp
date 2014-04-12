a938a37eb8df843c091b7b186a25cc82e37b05a7
Corbin Souffrant <souffra2@illinois.edu>
2014-04-11 19:09:29 -0500

    minor changes

--- parser.cpp
+++ parser.cpp
@@ -34,7 +34,7 @@ void parse_chart(std::string & input_file, Song & input_song) {
 	uint32_t row_position = 0;
 	uint32_t cur_char = 0;
 
-	//Begin by checking the headers
+	//Begin by checking the metadata
 	if(boost::starts_with(line, "t =")) {
 	    input_song.bpm = add_header_var(line);
 	    if(input_song.bpm != 0 && input_song.beats != 0) {
@@ -42,7 +42,7 @@ void parse_chart(std::string & input_file, Song & input_song) {
 					  (input_song.note_value / 4));
 	    }
 	} else if(boost::starts_with(line, "o =")) {
-	    offset_timer = add_header_var(line);
+	    offset_timer += add_header_var(line);
 	} else if(boost::starts_with(line, "b =")) {
 	    input_song.beats = add_header_var(line);
 	    if(input_song.bpm != 0 && input_song.beats != 0) {
@@ -64,8 +64,9 @@ void parse_chart(std::string & input_file, Song & input_song) {
 	    for(note_seq = line.begin(); note_seq != line_end;
 		cur_char = utf8::next(note_seq,line_end)) {
 
+		//Checks for vertical bar, signifying parsing of timing.
 		if(cur_char == 124) {
-		    uint32_t note_value = utf8::distance(note_seq, line_end) - 1;
+		    uint32_t note_value = utf8::distance(note_seq, line_end)-1;
 		    note_timing = true;
 
 		    if(note_value == 0) {
@@ -73,6 +74,8 @@ void parse_chart(std::string & input_file, Song & input_song) {
 		    }
 
 		    offset_increment = input_song.note_offset / note_value;
+
+		  //If not in timing, then reading in note ordering
 		} else if(note_timing == false) {
 		    int32_t order_num = unicode_to_order(cur_char);
 
@@ -82,6 +85,7 @@ void parse_chart(std::string & input_file, Song & input_song) {
 		    } else if(cur_char == 9633) {
 			row_position++;
 		    }
+		  //In timing, assume either a note or a rest.
 		} else {
 		    int32_t order_num = unicode_to_order(cur_char);
 
@@ -93,7 +97,8 @@ void parse_chart(std::string & input_file, Song & input_song) {
 		}
 	    }
 
-	    note_row = (note_row + 1) % input_song.beats;
+	    //Only reads in one full 4x4 grid at a time.
+	    note_row = (note_row + 1) % 4;
 
 	    if(note_row == 0) {
 		add_notes(input_song, note_order, positions);
@@ -112,7 +117,9 @@ void parse_chart(std::string & input_file, Song & input_song) {
     step_chart.close();
 }
 
-void add_notes(Song & input_song, int32_t note_order[][4], int32_t * positions) {
+void add_notes(Song & input_song,
+	       int32_t note_order[][4],
+	       int32_t * positions) {
     for(int32_t pos_idx = 0; pos_idx < 16; pos_idx++) {
 	for(int32_t note_row = 0; note_row < 4; note_row++) {
 	    for(int32_t note_col = 0; note_col < 4; note_col++) {
@@ -120,7 +127,9 @@ void add_notes(Song & input_song, int32_t note_order[][4], int32_t * positions) 
 		uint32_t time = positions[pos_idx];
 
 		if(note - 1 == pos_idx) {
-		    input_song.note_position.push_back(std::make_tuple(note_row, note_col, time));
+		    input_song.note_position.push_back(std::make_tuple(note_row,
+								       note_col,
+								       time));
 		}
 	    }
 	}
