d75e6b46d0f5625a9cad259f742590f5782d97c4
Corbin Souffrant <souffra2@illinois.edu>
2014-05-02 20:45:27 -0500

    Added licenses to all the files, fixed all but 1 errors with all cpplint.

--- src/audio.cpp
+++ src/audio.cpp
@@ -1,3 +1,15 @@
+/*
+Copyright (c) 2014 Corbin Souffrant
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <SDL2/SDL_mixer.h>
 #include <string>
 
@@ -12,9 +24,10 @@ Audio::~Audio() {
 }
 
 void Audio::init() {
-    // Initialize SDL_mixer
-    if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-	printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+    // Initialize SDL_mixer.
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n",
+                   Mix_GetError());
     }
 }
 
@@ -23,8 +36,8 @@ void Audio::play_song(std::string const & song_title) {
         std::string("/") + song_title + std::string(".mp3");
 
     song = Mix_LoadMUS(song_file.c_str());
-    if(song == NULL) {
-	printf("Failed to load music! SDL_mixer Error: %s\n", Mix_GetError());
+    if (song == NULL) {
+        printf("Failed to load music! SDL_mixer Error: %s\n", Mix_GetError());
     }
 
     Mix_PlayMusic(song, -1);
--- src/display.h
+++ src/display.h
@@ -1,3 +1,18 @@
+/*
+Copyright (c) 2014 Corbin Souffrant
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SDL code modified from
+https://github.com/Twinklebear/TwinklebearDev-Lessons
+*/
+
 #include <SDL2/SDL_image.h>
 #include <string>
 
@@ -104,19 +119,3 @@ class Display {
 };
 
 #endif  // SRC_DISPLAY_H_
-
-/*
-SDL code from TwinklebearDev-Lessons.
-https://github.com/Twinklebear/TwinklebearDev-Lessons
-
-License:
-The MIT License (MIT)
-
-Copyright (c) 2013 Will Usher
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
